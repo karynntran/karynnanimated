c5f8c8bf93fb9d25b8192b1570a0175c6e99679c
karynntran <karynn.tran@gmail.com>
2017-04-04 22:27:49 -0400

    update webpack cofig

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+// var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
   entry: [
     './src/index.js'
@@ -8,21 +10,26 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      },
+      {
+          test: /\.json$/,
+          loader: 'json'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
-    },
-    {
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader']
-    }
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx', '.json', '.scss']
   },
   devServer: {
     historyApiFallback: true,
